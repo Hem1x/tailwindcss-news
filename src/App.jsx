8c2c6b9e68306160d6d65867321227540b04095d
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-06-17 07:55:31 +0300

    work with Fonts and Colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='App'>
-    </div>
+    <header>
+      <a className='uppercase font-bold text-purple-800' href="#">TailwindCSS news</a>
+
+      <nav>
+        <ul className="text-gray-500 font-semibold" >
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
